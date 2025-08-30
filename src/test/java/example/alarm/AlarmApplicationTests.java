60ffe86e3ea7febcb11af8d65a67a0a274aa172a
hyun <apfhd0257@naver.com>
2025-08-30 21:48:58 +0900

    init

---
+++ src/test/java/example/alarm/AlarmApplicationTests.java
@@ -0,0 +1,13 @@
+package example.alarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
